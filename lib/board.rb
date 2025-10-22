13f339f4b3de55e9778c040354bb09c092be2c05
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-10-22 17:33:00 -0400

    Board: Implement #win?

--- lib/board.rb
+++ lib/board.rb
@@ -30,6 +30,16 @@ class Board
     [row_index, column_index]
   end
 
+  def win?(start_coordinates)
+    row_index, column_index = *start_coordinates
+    player_token = @grid.dig(row_index, column_index)
+    return false if player_token.nil?
+
+    true if win_horizontal?(row_index, player_token) ||
+            win_vertical?(column_index, player_token) ||
+            win_diagonal?(row_index, column_index, player_token)
+  end
+
   private
 
   def print_row(row)
@@ -39,5 +49,60 @@ class Board
       print "#{symbol} "
     end
     puts '|'
+  end
+
+  def win_horizontal?(row_index, player_token)
+    row = @grid[row_index]
+
+    valid_row?(row, player_token)
+  end
+
+  def win_vertical?(column_index, player_token)
+    column = @grid.collect { |row| row[column_index] }
+
+    valid_row?(column, player_token)
+  end
+
+  def win_diagonal?(row_index, column_index, player_token)
+    forward_anchor = find_anchor(row_index, column_index, -1, -1)
+    backward_anchor = find_anchor(row_index, column_index, -1, 1)
+
+    forward_diagonal = collect_diagonal(*forward_anchor, 1, 1)
+    backward_diagonal = collect_diagonal(*backward_anchor, 1, -1)
+
+    valid_row?(forward_diagonal, player_token) || valid_row?(backward_diagonal, player_token)
+  end
+
+  def find_anchor(row_index, column_index, row_step, column_step)
+    while valid_coordinates?(row_index + row_step, column_index + column_step)
+      row_index += row_step
+      column_index += column_step
+    end
+
+    [row_index, column_index]
+  end
+
+  def collect_diagonal(row_index, column_index, row_step, column_step)
+    values = []
+
+    while valid_coordinates?(row_index, column_index)
+      values.push(@grid[row_index][column_index])
+      row_index += row_step
+      column_index += column_step
+    end
+
+    values
+  end
+
+  def valid_coordinates?(row_index, column_index)
+    row_index.between?(0, 5) && column_index.between?(0, 6)
+  end
+
+  def valid_row?(token_values, player_token)
+    token_values.each_cons(4) do |group|
+      return true if group.all?(player_token)
+    end
+
+    false
   end
 end
