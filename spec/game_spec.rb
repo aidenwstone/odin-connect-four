4fa342edd77b107877dc56a885fa99542f53daca
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-10-24 20:08:48 -0400

    Write #player_input tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,6 +3,51 @@
 require_relative '../lib/game'
 
 describe Game do
+  describe '#player_input' do
+    subject(:game_input) { described_class.new }
+
+    let(:error_message) { 'Input error! Please enter a number from 1 to 7:' }
+    let(:input_valid) { '4' }
+    let(:input_word) { 'four' }
+    let(:input_char) { '$' }
+
+    context 'when user provides valid input' do
+      before do
+        allow(game_input).to receive(:puts)
+        allow(game_input).to receive(:gets).and_return(input_valid)
+      end
+
+      it 'stops loop and does not display error message' do
+        game_input.player_input
+        expect(game_input).not_to have_received(:puts).with(error_message)
+      end
+    end
+
+    context 'when user provides invalid input once, followed by valid input' do
+      before do
+        allow(game_input).to receive(:puts)
+        allow(game_input).to receive(:gets).and_return(input_word, input_valid)
+      end
+
+      it 'complete loop and displays error message once' do
+        game_input.player_input
+        expect(game_input).to have_received(:puts).with(error_message).once
+      end
+    end
+
+    context 'when user provides invalid input twice, followed by valid input' do
+      before do
+        allow(game_input).to receive(:puts)
+        allow(game_input).to receive(:gets).and_return(input_word, input_char, input_valid)
+      end
+
+      it 'complete loop and displays error message twice' do
+        game_input.player_input
+        expect(game_input).to have_received(:puts).with(error_message).twice
+      end
+    end
+  end
+
   describe '#process_input' do
     subject(:game_verify) { described_class.new }
 
